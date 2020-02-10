3e01ee010c07ef58d8913c1df1239afc09d35d27
Vegar Andreas Bergum <vegar.andreas@gmail.com>
2020-02-10 14:24:53 +0000

    feat #2: super-class for models

---
+++ tfn/models/model.py
@@ -0,0 +1,10 @@
+class Model:
+    def __init__(self):
+        pass
+
+    def fit(self, X, y):
+        raise Exception("NotImplementedException")
+
+    def predict(self, X):
+        raise Exception("NotImplementedException")
+
