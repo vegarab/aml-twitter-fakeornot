d23a9950be3597db7e6b2eba8f90628fef2cbed6
Adam <aghoumrassi@hotmail.co.uk>
2020-05-05 20:22:38 +0100

    Created main file

--- tfn/__init__.py
+++ tfn/__init__.py
@@ -2,3 +2,4 @@ from pkg_resources import resource_filename
 
 TRAIN_FILE = resource_filename(__name__, 'data/train.csv')
 GLOVE_25_FILE = resource_filename(__name__, 'misc/glove.twitter.27B.25d.txt')
+GLOVE_25_WV_FILE = resource_filename(__name__, 'misc/glove_25.wv')
